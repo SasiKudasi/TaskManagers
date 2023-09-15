f502fd843233f1edc78b8658c762b81138c6f81d
SasaiKudasai <sasha.mittal@yandex.ru>
2023-09-15 16:03:40 +0300

    Добавление класса Result

---
+++ Result.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using TaskManager;
+
+public static class Result
+{
+    private static Tasks _task = new Tasks();    
+    	
+        public static string GetTaskStatus()
+        {
+            Console.WriteLine("Выберите приоритет задачи:");
+            Console.WriteLine("1. Высокий");
+            Console.WriteLine("2. Средний");
+            Console.WriteLine("3. Низкий");
+
+            var key = Console.ReadKey();
+
+            if (ChousePrioruty(key) == Priority.LOW)
+            {
+                return _task.Priority = "Низкий";
+            }
+            if (ChousePrioruty(key) == Priority.HIGHT)
+            {
+                return _task.Priority = "Высокий";
+            }
+            if (ChousePrioruty(key) == Priority.MEDIUM)
+            {
+                return _task.Priority = "Средний";
+            }
+            return _task.Priority;
+        }
+
+        public static string TaskReady()
+        {
+            Console.WriteLine("Выполнить задачу?");
+            Console.WriteLine("1. Задача выполнена");
+            Console.WriteLine("2. Задача не выполнена");
+
+            var key = Console.ReadKey();
+            if (CheckStatus(key) == Status.IS_DONE)
+            {
+                _task.Status = "Выполнено";
+                return _task.Status;
+            }
+            if (CheckStatus(key) == Status.ISNT_DONE)
+            {
+                _task.Status = "Не выполнено";
+                return _task.Status;
+            }
+            return _task.Status;
+        }
+
+        private static Status CheckStatus(ConsoleKeyInfo key)
+        {
+            Status result = Status.ISNT_DONE;
+            switch (key.Key)
+            {
+                case ConsoleKey.D1:
+                    result = Status.IS_DONE;
+                    break;
+                case ConsoleKey.D2:
+                    result = Status.ISNT_DONE;
+                    break;
+            }
+            return result;
+        }
+
+        public static Priority ChousePrioruty(ConsoleKeyInfo key)
+        {
+            Priority result = Priority.MEDIUM;
+            switch (key.Key)
+            {
+                case ConsoleKey.D1:
+                    result = Priority.HIGHT;
+                    break;
+                case ConsoleKey.D2:
+                    result = Priority.MEDIUM;
+                    break;
+                case ConsoleKey.D3:
+                    result = Priority.LOW;
+                    break;
+            }
+            return result;
+        }
+
+}
+
--- TasksManager.cs
+++ TasksManager.cs
@@ -4,9 +4,8 @@ using ConsoleApp1;
 namespace TaskManager
 {
     public class TasksManager
-    {
-       
-
+    {        
+        
         private static Tasks _task = new Tasks();
      
         private static Dictionary<int, Tasks> _taskDictionary = new Dictionary<int, Tasks>();
@@ -14,11 +13,11 @@ namespace TaskManager
         private static AppDbContext _appDbContext = new AppDbContext();
                        
         internal static void AddTask()
-        {
-            _task.Id = AddKey();
-            AddTaskName();            
-            GetTaskStatus();
-            TaskReady();            
+        {            
+           _task.Id = AddKey();
+           AddTaskName();
+           _task.Priority = Result.GetTaskStatus();
+           _task.Status =  Result.TaskReady();            
 
             _taskDictionary.Add(_task.Id, new Tasks( _task.TaskName, _task.Priority, _task.Status));
 
@@ -43,83 +42,7 @@ namespace TaskManager
             _task.TaskName = Console.ReadLine();
         }
         
-        public static string GetTaskStatus()
-        {
-            Console.WriteLine("Выберите приоритет задачи:");
-            Console.WriteLine("1. Высокий");
-            Console.WriteLine("2. Средний");
-            Console.WriteLine("3. Низкий");
-
-            var key = Console.ReadKey();
-
-            if (ChousePrioruty(key) == Priority.LOW)
-            {
-              return  _task.Priority = "Низкий";
-            }
-            if (ChousePrioruty(key) == Priority.HIGHT)
-            {
-              return  _task.Priority = "Высокий";
-            }
-            if (ChousePrioruty(key) == Priority.MEDIUM)
-            {
-              return  _task.Priority = "Средний";
-            }
-            return _task.Priority;
-        }
-
-        public static string TaskReady ()
-        {
-            Console.WriteLine("Выполнить задачу?");
-            Console.WriteLine("1. Задача выполнена");
-            Console.WriteLine("2. Задача не выполнена");
-
-            var key = Console.ReadKey();
-            if (CheckStatus(key) == Status.IS_DONE)
-            {
-                _task.Status = "Выполнено";
-                return _task.Status;
-            } 
-            if(CheckStatus(key) == Status.ISNT_DONE)
-            {
-                _task.Status = "Не выполнено";
-                return _task.Status;
-            }
-            return _task.Status;
-        }
-
-        private static Status CheckStatus(ConsoleKeyInfo key)
-        {
-            Status result = Status.ISNT_DONE;
-            switch (key.Key)
-            {
-                case ConsoleKey.D1:
-                    result = Status.IS_DONE;
-                break; 
-                case ConsoleKey.D2:
-                    result = Status.ISNT_DONE; 
-                break;
-            }
-            return result;
-        }
-
-        public static Priority ChousePrioruty(ConsoleKeyInfo key)
-        {
-            Priority result = Priority.MEDIUM;
-            switch (key.Key)
-            {
-                case ConsoleKey.D1:
-                      result = Priority.HIGHT;
-                break;
-                case ConsoleKey.D2:
-                    result = Priority.MEDIUM;
-                break;
-                case ConsoleKey.D3:
-                    result = Priority.LOW; 
-                break;           
-            }
-            return result;           
-        }
-
+       
         public static void SortPriority (string text)
         {
             var priority = _appDbContext.Tasks.Where(task => task.Priority == text);
@@ -221,12 +144,12 @@ namespace TaskManager
             if (key1 == 2)
             {
                 Console.WriteLine("Укажите приоритет");
-                taskUpdate.Priority =  GetTaskStatus();
+                taskUpdate.Priority =  Result.GetTaskStatus();
             }
             if(key1 == 3)
             {
                 Console.WriteLine("Укажите готовность");
-                taskUpdate.Status = TaskReady();
+                taskUpdate.Status = Result.TaskReady();
             }
             _appDbContext.SaveChanges();
             BrowseAllTask();
